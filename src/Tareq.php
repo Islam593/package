2bf99edfd03d4f1daa9e213d219d4acb86571964
tareqislam <blnre.tareq@gmail.com>
2022-01-28 21:04:22 +0600

    load files

---
+++ src/Tareq.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Islamtareqgit\Package;
+Class Tareq{
+
+    
+    /**
+     * Make slug
+     */
+
+
+     public static function slugMake($title)
+     {
+       $lowerdata = strtolower($title);
+       return str_replace(' ', '-', $lowerdata);
+     }
+
+     // Array to JSON Convert
+
+     public function jsonEnc(Array $arr)
+     {
+       return json_encode($arr);
+     }
+
+
+     //  JSON to array Convert
+
+     public static function jsondecode(String $str, $type = false)
+     {
+       return json_decode($str, $type);
+     }
+
+
+     // Random number generate
+
+     public function randNum()
+     {
+      return rand();
+     }
+
+     // Unique name
+
+     public function uname($ext)
+     {
+       return md5(time().rand()).'.'.$ext;
+     }
+
+}
+
+
+
+
+
+?>
